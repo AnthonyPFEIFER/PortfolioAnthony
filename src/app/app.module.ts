669b9e2f74cdcaa93ca3faac6460c5371a892c48
AnthonyPFEIFER <anthhony.pfeifer@gmail.com>
2022-10-06 09:50:40 +0200

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FootbarComponent } from './components/footbar/footbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    SidebarComponent,
+    FootbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
