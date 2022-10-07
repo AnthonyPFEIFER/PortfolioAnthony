47b6b585c8ab6d03503bdc3b3e2fd5e44bd84a34
AnthonyPFEIFER <anthhony.pfeifer@gmail.com>
2022-10-07 13:48:09 +0200

    Dashboard nearly finished, created components for skills etc and scss cleaned

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ExperiencesComponent } from './components/experiences/experiences.component';
+import { FormationsComponent } from './components/formations/formations.component';
+import { InformationsComponent } from './components/informations/informations.component';
+import { TechnologiesComponent } from './components/technologies/technologies.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-  { path: 'dashboard', component: DashboardComponent}
+  { path: 'dashboard', component: DashboardComponent},
+  { path: 'experiences', component: ExperiencesComponent},
+  { path: 'formations', component: FormationsComponent},
+  { path: 'technologies', component: TechnologiesComponent},
+  { path: 'informations', component: InformationsComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { FootbarComponent } from './components/footbar/footbar.component';
+import { ExperiencesComponent } from './components/experiences/experiences.component';
+import { FormationsComponent } from './components/formations/formations.component';
+import { TechnologiesComponent } from './components/technologies/technologies.component';
+import { InformationsComponent } from './components/informations/informations.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     SidebarComponent,
-    FootbarComponent
+    FootbarComponent,
+    ExperiencesComponent,
+    FormationsComponent,
+    TechnologiesComponent,
+    InformationsComponent
   ],
   imports: [
     BrowserModule,
